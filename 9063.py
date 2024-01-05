267c2e6c654d0ef4953711d382d21be7d2de589d
92nodo-dev <92nodo30@gmail.com>
2024-01-05 14:05:10 +0900

    Update 9063.py

--- 9063.py
+++ 9063.py
@@ -1,11 +1,18 @@
-n=int(input())
-a, b = map(int, input().split())
-if n<3:print(0);exit()
-minX,minY,maxX,maxY=a,b,a,b
-for i in range(n-1):
-    a,b=map(int, input().split())
-    minX=min(minX,a)
-    minY = min(minY, b)
-    maxX = max(maxX, a)
-    maxY = max(maxY, b)
-print((maxX-minX)*(maxY-minY))
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+max_x = -100000
+max_y = -100000
+min_x = 100000
+min_y = 100000
+
+for _ in range(N):
+    x, y = map(int, input().split())
+
+    max_x = max(max_x, x)
+    min_x = min(min_x, x)
+    max_y = max(max_y, y)
+    min_y = min(min_y, y)
+
+print(abs(max_x-min_x) * abs(max_y-min_y))
